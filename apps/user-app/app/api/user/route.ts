992a44cee13d3dbee3e8147e4273db69e5f1b4c0
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-08 19:33:44 +0530

    Implement Prisma database and verify functionality with basic route

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import db from "@repo/pocketflow/client";
+
+export const GET = async () => {
+  try {
+    await db.user.create({
+      data: {
+        email: "abc@gmail.com",
+        number: "123456789",
+        password: "87654321"
+      },
+    });
+    return NextResponse.json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "Failed to create user",
+      },
+      { status: 500 }
+    );
+  }
+};
