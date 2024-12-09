853a0b4102b8a65f944e13e9a899c0a113e47eff
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-09 13:50:02 +0530

    Refactor project structure for better maintainability and fewer bugs

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
-type Props = {};
-
-const Page = (props: Props) => {
-
-  return <div>page</div>;
-};
-
-export default Page;
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
+}
