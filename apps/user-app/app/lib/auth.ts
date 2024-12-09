61c9c427bf480bdbcb34fd3ab6127257448db3ae
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-09 18:04:39 +0530

    Initialize bank_webhook project to interact with bank APIs

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,63 +1,75 @@
 import db from "@repo/db/client";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
 export const authOptions = {
-    providers: [
-      CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            phone: { label: "Mobile number", type: "text", placeholder: "Number", required: true },
-            password: { label: "Password", type: "password", required: true }
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        phone: {
+          label: "Mobile number",
+          type: "text",
+          placeholder: "Number",
+          required: true,
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+          required: true,
+        },
+      },
+      async authorize(credentials: any) {
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+        const existingUser = await db.user.findFirst({
+          where: {
+            number: credentials.phone,
           },
-          async authorize(credentials: any) {
-            const hashedPassword = await bcrypt.hash(credentials.password, 10);
-            const existingUser = await db.user.findFirst({
-                where: {
-                    number: credentials.phone
-                }
-            });
+        });
 
-            if (existingUser) {
-                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                if (passwordValidation) {
-                    return {
-                        id: existingUser.id.toString(),
-                        name: existingUser.name,
-                        number: existingUser.number
-                    }
-                }
-                return null;
-            }
-
-            try {
-                const user = await db.user.create({
-                    data: {
-                        number: credentials.phone,
-                        password: hashedPassword
-                    }
-                });
-            
-                return {
-                    id: user.id.toString(),
-                    name: user.name,
-                    number: user.number
-                }
-            } catch(e) {
-                console.error(e);
-            }
-
-            return null
-          },
-        })
-    ],
-    secret: process.env.JWT_SECRET || "secret",
-    callbacks: {
-        async session({ token, session }: any) {
-            session.user.id = token.sub
-
-            return session
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              number: existingUser.number,
+            };
+          }
+          return null;
         }
-    }
-  }
-  
+
+        try {
+          const user = await db.user.create({
+            data: {
+              number: credentials.phone,
+              password: hashedPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            number: user.number,
+          };
+        } catch (e) {
+          console.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    async session({ token, session }: any) {
+      session.user.id = token.sub;
+
+      return session;
+    },
+  },
+};
