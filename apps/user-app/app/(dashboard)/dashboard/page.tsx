bba3ba416748e76761965a22c8b0f985b3a3834e
ToseebNadaf <toseebnadaf@gmail.com>
2025-01-05 11:35:33 +0530

    Refactor: Added recharts library in dashboard page to generate graph

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,44 @@
-import { Quate } from "../../../components/Quate";
-export default function () {
+import TransactionGraph from "../../../components/TransactionGraph";
+import getAllTransactions from "../../lib/getTxn";
+import { BalanceCard } from "../../../components/BalanceCard";
+import getBalance from "../../lib/getBalance";
+import isAuth from "../isAuth";
+import { redirect } from "next/navigation";
+
+async function DashboardPage() {
+  const user = await isAuth();
+  if (!user) {
+    console.log("User not authenticated");
+    redirect("api/auth/signin");
+  }
+  const transaction = await getAllTransactions();
+  const balance = await getBalance();
+
   return (
-    <div>
-      <Quate />
+    <div className="w-full p-5 min-h-screen">
+      <div className="text-4xl text-[#6a51a6] font-bold mb-6">Dashboard</div>
+
+      <div className="bg-white rounded-lg shadow-md p-6 mb-8">
+        <h2 className="text-2xl font-semibold text-gray-800">
+          Current Balance
+        </h2>
+        <p className="text-3xl text-[#6a51a6] font-bold">
+          {(balance.amount / 100).toFixed(2)} RS
+        </p>
+      </div>
+
+      <div className="bg-white rounded-lg shadow-md p-6 mb-8">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          Transaction History
+        </h2>
+        <TransactionGraph transactions={transaction} />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <BalanceCard amount={balance.amount} locked={balance.locked} />
+      </div>
     </div>
   );
 }
+
+export default DashboardPage;
---
+++ apps/user-app/app/(dashboard)/isAuth.tsx
@@ -0,0 +1,13 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../lib/auth";
+
+export default async function isAuth() {
+  const session = await getServerSession(authOptions);
+  if (session?.user?.id) {
+    return true;
+  } else {
+    return false;
+  }
+}
